168f32c7ec59c31369abbb52b905b7081b1f31ea
Vincze Gábor <vinczegabor1981@gmail.com>
2025-06-30 11:37:42 +0200

    add 30 zeros, set segment registers up

--- src/boot.asm
+++ src/boot.asm
@@ -1,7 +1,25 @@
 ORG 0x7C00
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+; BIOS PARAMETER BLOCK
+times 30 db 0
+
 start:
+    jmp 0x00:setup
+
+setup:
+    cli
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
     mov si, message
     call print
 
@@ -24,7 +42,7 @@ print_char:
     int 0x10
     ret
 
-message: db 'Hello Loosers!', 0
+message: db 'alpha beta', 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
