c5e074347b3439db9f1b95c7ec85cc11a4ab3440
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-02 17:34:16 +0200

    add text and 1 sector of zeros to boot.bin

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ DEPENDENCIES =
 
 build: clean
 	nasm -f bin ./src/boot.asm -o ./bin/boot.bin
+	dd if=./message.txt >> ./bin/boot.bin
+	dd if=/dev/zero bs=512 count=1 >> ./bin/boot.bin
 
 run:
 	qemu-system-i386 -hda ./bin/boot.bin
--- src/boot.asm
+++ src/boot.asm
@@ -1,13 +1,6 @@
 ORG 0x7C00
 BITS 16
 
-%macro print_char 1
-    mov ah, 0x0e
-    mov al, %1
-    mov bx, 0
-    int 0x10
-%endmacro
-
 _start:
     jmp short start
     nop
@@ -18,12 +11,6 @@ times 30 db 0
 start:
     jmp 0x00:setup
 
-handle_zero:
-    iret
-
-handle_one:
-    iret
-
 setup:
     cli
     mov ax, 0
@@ -33,16 +20,15 @@ setup:
     mov sp, 0x7C00
     sti
 
+; main code
+    mov ah, 0x0e
+    mov al, 'X'
     mov bx, 0
-    mov word [bx], handle_zero
-    mov word [bx+2], 0
-
-    mov word [bx+4], handle_one
-    mov word [bx+6], 0
+    int 0x10
+; end
 
     jmp $
 
-result:
 times 510 - ($ - $$) db 0
 
 dw 0xAA55
