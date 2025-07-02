093c0240ef7cabab21f47a838eca170fce0ceb09
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-02 06:41:46 +0200

    create interrupt-handlers in real-address mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,13 @@
 ORG 0x7C00
 BITS 16
 
+%macro print_char 1
+    mov ah, 0x0e
+    mov al, %1
+    mov bx, 0
+    int 0x10
+%endmacro
+
 _start:
     jmp short start
     nop
@@ -11,6 +18,12 @@ times 30 db 0
 start:
     jmp 0x00:setup
 
+handle_zero:
+    iret
+
+handle_one:
+    iret
+
 setup:
     cli
     mov ax, 0
@@ -20,29 +33,16 @@ setup:
     mov sp, 0x7C00
     sti
 
-    mov si, message
-    call print
+    mov bx, 0
+    mov word [bx], handle_zero
+    mov word [bx+2], 0
+
+    mov word [bx+4], handle_one
+    mov word [bx+6], 0
 
     jmp $
 
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-
-.done:
-    ret
-
-print_char:
-    mov ah, 0x0e
-    int 0x10
-    ret
-
-message: db 'alpha beta', 0
-
+result:
 times 510 - ($ - $$) db 0
+
 dw 0xAA55
