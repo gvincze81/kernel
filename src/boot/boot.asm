621acf257c4d5b553b03119e7c2084b9e31f83d7
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-04 02:56:30 +0200

    enable the A20 line

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -79,6 +79,12 @@ load_32:
     mov ebp, 0x00200000
     mov esp, ebp
 
+    ; Enable the A20 line
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+    ; End
+
     jmp $
 
 times 510 - ($ - $$) db 0
