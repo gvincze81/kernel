92ffa8b3deef3f028b4080e5ffc7cbd770e3c5b2
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-05 14:53:12 +0200

    implement terminal_make_char

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,12 @@
 #include "kernel.h"
 
+uint16_t terminal_make_char(char ch, char color)
+{
+    return (color << 8) | ch;
+}
+
 void kernel_main()
 {
+    uint16_t *video_mem = (uint16_t*)(VIDEO_MEM);
+    video_mem[0] = terminal_make_char('A', 4);
 }
--- src/kernel.h
+++ src/kernel.h
@@ -1,6 +1,13 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
+
 void kernel_main();
+
+#define VIDEO_MEM 0xB8000
+
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
 
 #endif
