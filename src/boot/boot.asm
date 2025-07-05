4e486d660d9af90d48c0fb32cfd05158eb893f40
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-05 09:46:16 +0200

    boot.asm not working

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -5,10 +5,10 @@ CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
 %define PROTECTION_ENABLE 1o
+%define KERNEL_EFFECTIVE_ADDRESS 0x100000
 
-_start:
-    jmp short start
-    nop
+jmp short start
+nop
 
 ; BIOS PARAMETER BLOCK
 times 30 db 0
@@ -32,7 +32,7 @@ setup:
     or eax, PROTECTION_ENABLE
     mov cr0, eax
     ; Processor is in protected mode from this point
-    jmp CODE_SEG:load_32 ; File offset 0x67
+    jmp CODE_SEG:load_32
 
 ; GDT
 
@@ -67,9 +67,74 @@ gdt_descriptor:
 
 [BITS 32]
 
+; Load kernel to memory
 load_32:
-    jmp $
-; End
+    mov eax, 1
+    mov ecx, 100
+    mov edi, 0x100000
+    call ata_lba_read
+    jmp CODE_SEG:KERNEL_EFFECTIVE_ADDRESS
+
+ata_lba_read:
+    mov ebx, eax ; Backup the LBA
+    ; Send highest 8 bits to hard disk controller
+    shr eax, 24
+    or eax, 0xE0 ; Selects the master drive
+    mov dx, 0x01F6
+    out dx, al
+    ; Finished sending highest 8 bits
+
+    ; Send number of total sectors
+    mov eax, ecx
+    mov dx, 0x01F2
+    out dx, al
+    ; End
+
+    ; Send bottom 8 bits
+    mov eax, ebx ; Restore original value of LBA
+    mov dx, 0x01F3
+    out dx, al
+    ; End
+
+    ; Send more bits
+    mov dx, 0x01F4
+    mov eax, ebx
+    shr eax, 8
+    out dx, al
+    ; End
+
+    ; Send 8 bits at index 2(third from bottom)
+    mov dx, 0x01F5
+    mov eax, ebx
+    shr eax, 16
+    out dx, al
+    ; End
+
+    mov dx, 0x01F7
+    mov al, 0x20
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+%define TEST_BIT 8
+; Checking if we need to read
+.try_again:
+
+    mov dx, 0x01F7
+    in al, dx
+    test al, TEST_BIT
+    je .try_again
+
+; We need to read 256 sectors at a time
+    mov ecx, 256
+    mov dx, 0x01F0
+    rep insw
+    pop ecx
+    loop .next_sector
+    ; Finished reading all sectors
+    ret
 
 times 510 - ($ - $$) db 0
 
