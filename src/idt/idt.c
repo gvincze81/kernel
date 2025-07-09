e65dd468be0b12df2679a08a056ba1bb7561ff3b
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-09 09:20:04 +0200

    set no_interrupt for all idt desriptors and set int21h

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -3,15 +3,24 @@
 #include "kernel.h"
 
 #include "memory/memory.h"
+#include "io/io.h"
 
 struct idt_desc idt_descriptors[TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
 void idt_load(struct idtr_desc *ptr);
+void int21h();
+void no_interrupt();
 
-void idt_zero()
+void no_interrupt_handler()
 {
-    print("Interrupt 0");
+    outb(0x20, 0x20);
+}
+
+void int21h_handler()
+{
+    print("Keyboard pressed\n");
+    outb(0x20, 0x20);
 }
 
 void idt_set(uint16_t interrupt_no, void *address)
@@ -30,7 +39,12 @@ void idt_init()
     idtr_descriptor.limit = sizeof(idt_descriptors) - 1;
     idtr_descriptor.base = (uint32_t)idt_descriptors;
 
-    idt_set(0, idt_zero);
+    for (int i = 0; i < TOTAL_INTERRUPTS; i++)
+    {
+        idt_set(i, no_interrupt);
+    }
+
+    idt_set(0x21, int21h);
 
     // Load the IDT
     idt_load(&idtr_descriptor);
