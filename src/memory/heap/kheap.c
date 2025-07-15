f788ac2755209b4d303e7e1fab7fe596a3b81abf
Vincze Gábor <vinczegabor1981@gmail.com>
2025-07-15 08:01:07 +0200

    integrate heap into kheap

--- src/memory/heap/kheap.c
+++ src/memory/heap/kheap.c
@@ -23,3 +23,13 @@ void kheap_init()
         print("Failed to create heap");
     }
 }
+
+void *kmalloc(size_t size)
+{
+    return heap_malloc(&kernel_heap, size);
+}
+
+void kfree(void *ptr)
+{
+    heap_free(&kernel_heap, ptr);
+}
